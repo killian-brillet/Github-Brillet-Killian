75ed684a6edb90467191e5a85d95a2b7ff76edfb
killian-brillet <74669539+killian-brillet@users.noreply.github.com>
2020-12-19 15:14:30 +0100

    19/12/20 1v1 Fonctionnel

--- Jeu RPG HTML Killian Brillet/javascript.js
+++ Jeu RPG HTML Killian Brillet/javascript.js
@@ -11,44 +11,61 @@ var valeurheal = 20 ;
 var atkmonstre = 10 ;
 
 function verifpv(){
-	if (pvmonstre <= 0);
-		texte.innerHTML = "Vous avez gagné, vous êtes un boss";
-	if (pvperso <= 0);
-		texte.innerHTML = "Vous avez perdu, vous etes une grosse merde";
+	if (pvmonstre <= 0){
+		texte.innerHTML = "Vous avez gagné";
+	}
+	if (pvperso <= 0){
+		texte.innerHTML = "Vous avez perdu";
+	}
 }
 
-function tour(){
-
-	atk.onclick = function(){
-		texte.innerHTML = "Vous attaquez, <br> Quellle est votre prochaine action?";
-		pvmonstre = pvmonstre-atkperso;
+atk.onclick = function(){
+	texte.innerHTML = "Vous attaquez, <br> Quellle est votre prochaine action?";
+	if (pvperso > 0, pvmonstre > 0){
+		if (pvmonstre  <= atkperso){
+			pvmonstre = 0;
+		}
+		else{
+			pvmonstre = pvmonstre-atkperso;
+		}
 		document.getElementById("pvmon").innerHTML=pvmonstre;
-		pvperso = pvperso-atkmonstre;
+		if (pvperso <= atkmonstre){
+			pvperso = 0;
+		}
+		else{
+			pvperso = pvperso-atkmonstre;	
+		}
 		document.getElementById("affichagepv").innerHTML=pvperso;
 	}
+	verifpv()
+}
 
-	def.onclick = function(){
+def.onclick = function(){
+	if (pvperso > 0, pvmonstre > 0){
 		texte.innerHTML = "Vous defendez, <br> Quellle est votre prochaine action?";
-		pvperso = pvperso-(atkmonstre*defperso);
+		if (pvperso <= (atkmonstre*defperso)){
+			pvperso = 0
+		}
+		else{
+			pvperso = pvperso-(atkmonstre*defperso);
+		}
 		document.getElementById("affichagepv").innerHTML=pvperso;
 	}
+	verifpv()
+}
 
-	heal.onclick = function(){
+heal.onclick = function(){
+	if (pvperso > 0, pvmonstre > 0){
 		if (manaperso >= 10){
 			pvperso = pvperso+(valeurheal-atkmonstre);
 			manaperso = manaperso-10;
 			document.getElementById("affichagepv").innerHTML=pvperso;
 			document.getElementById("affichagemana").innerHTML=manaperso;
 			texte.innerHTML = "Vous vous soignez, <br> Quellle est votre prochaine action?";
+			verifpv()
 		}
 		else{
 			texte.innerHTML = "Vous n'avez pas assez de mana";
 		}
 	}
-
-tour();
-verifpv();
-
 }
-
-
