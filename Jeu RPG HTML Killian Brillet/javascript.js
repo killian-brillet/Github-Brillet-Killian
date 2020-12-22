4d25b869a52b62db08e5f89e8b77d58f95e62578
killian-brillet <74669539+killian-brillet@users.noreply.github.com>
2020-12-22 14:30:14 +0100

    1v1 fonctionnel

--- Jeu RPG HTML Killian Brillet/javascript.js
+++ Jeu RPG HTML Killian Brillet/javascript.js
@@ -10,76 +10,84 @@ var defperso = 0.5 ;
 var valeurheal = 20 ;
 var atkmonstre = 10 ;
 var boutonutilise = 0 ;
+var finpartie = 0 ;
 
 function verifpv(){
 	if (pvmonstre <= 0){
 		texte.innerHTML = "Vous avez gagné";
+		finpartie = 1;
 	}
 	if (pvperso <= 0){
 		texte.innerHTML = "Vous avez perdu";
+		finpartie = 1;
 	}
 }
 
 atk.onclick = function(){
-	if (pvperso > 0, pvmonstre > 0, boutonutilise != 1){
-		texte.innerHTML = "Vous attaquez, <br> Quellle est votre prochaine action?";
-		if (pvmonstre  <= atkperso){
-			pvmonstre = 0;
+	if (finpartie != 1 ){
+		if (pvperso > 0, pvmonstre > 0, boutonutilise != 1){
+			texte.innerHTML = "Vous attaquez, <br> Quellle est votre prochaine action?";
+			if (pvmonstre  <= atkperso){
+				pvmonstre = 0;
+			}
+			else{
+				pvmonstre = pvmonstre-atkperso;
+			}
+			document.getElementById("pvmon").innerHTML=pvmonstre;
+			if (pvperso <= atkmonstre){
+				pvperso = 0;
+			}
+			else{
+				pvperso = pvperso-atkmonstre;	
+			}
+			document.getElementById("affichagepv").innerHTML=pvperso;
 		}
-		else{
-			pvmonstre = pvmonstre-atkperso;
+		if (pvperso > 0, pvmonstre > 0, boutonutilise = 1){
+			texte.innerHTML = "Vous avez déja utilisé l'attaque au tour précédent, choisissez une autre attaque."
 		}
-		document.getElementById("pvmon").innerHTML=pvmonstre;
-		if (pvperso <= atkmonstre){
-			pvperso = 0;
-		}
-		else{
-			pvperso = pvperso-atkmonstre;	
-		}
-		document.getElementById("affichagepv").innerHTML=pvperso;
+		boutonutilise = 1
+		verifpv()
 	}
-	else{
-		texte.innerHTML = "Vous avez déja utilisé l'attaque au tour précédent, choisissez une autre attaque."
-	}
-	boutonutilise = 1
-	verifpv()
 }
 
 def.onclick = function(){
-	if (pvperso > 0, pvmonstre > 0, boutonutilise != 2){
-		texte.innerHTML = "Vous defendez, <br> Quellle est votre prochaine action?";
-		if (pvperso <= (atkmonstre*defperso)){
-			pvperso = 0
+	if (finpartie != 1 ){
+		if (pvperso > 0, pvmonstre > 0, boutonutilise != 2){
+			texte.innerHTML = "Vous defendez, <br> Quellle est votre prochaine action?";
+			if (pvperso <= (atkmonstre*defperso)){
+				pvperso = 0
+			}
+			else{
+				pvperso = pvperso-(atkmonstre*defperso);
+			}
+			document.getElementById("affichagepv").innerHTML=pvperso;
 		}
-		else{
-			pvperso = pvperso-(atkmonstre*defperso);
+		if (pvperso > 0, pvmonstre > 0, boutonutilise = 2){
+			texte.innerHTML = "Vous avez déja utilisé la defense au tour précédent, choisissez une autre attaque."
 		}
-		document.getElementById("affichagepv").innerHTML=pvperso;
+		boutonutilise = 2
+		verifpv()
 	}
-	else{
-		texte.innerHTML = "Vous avez déja utilisé la defense au tour précédent, choisissez une autre attaque."
-	}
-	boutonutilise = 2
-	verifpv()
 }
 
 heal.onclick = function(){
-	if (pvperso > 0, pvmonstre > 0, boutonutilise != 3){
-		if (manaperso >= 10){
-			pvperso = pvperso+(valeurheal-atkmonstre);
-			manaperso = manaperso-10;
-			document.getElementById("affichagepv").innerHTML=pvperso;
-			document.getElementById("affichagemana").innerHTML=manaperso;
-			texte.innerHTML = "Vous vous soignez, <br> Quellle est votre prochaine action?";
-			verifpv()
+	if (finpartie != 1 ){
+		if (pvperso > 0, pvmonstre > 0, boutonutilise != 3){
+			if (manaperso >= 10){
+				pvperso = pvperso+(valeurheal-atkmonstre);
+				manaperso = manaperso-10;
+				document.getElementById("affichagepv").innerHTML=pvperso;
+				document.getElementById("affichagemana").innerHTML=manaperso;
+				texte.innerHTML = "Vous vous soignez, <br> Quellle est votre prochaine action?";
+			}
+			else{
+				texte.innerHTML = "Vous n'avez pas assez de mana";
+			}
 		}
-		else{
-			texte.innerHTML = "Vous n'avez pas assez de mana";
+		if (pvperso > 0, pvmonstre > 0, boutonutilise = 3){
+			texte.innerHTML = "Vous avez déja utilisé le soin au tour précédent, choisissez une autre attaque."
 		}
+		boutonutilise = 3
+		verifpv()
 	}
-	else{
-		texte.innerHTML = "Vous avez déja utilisé le soin au tour précédent, choisissez une autre attaque."
-	}
-	boutonutilise = 3
-	verifpv()
 }
